04f5c7646c66bb6f284dd49843b69095c07ee426
Nathan-Kimm <nathanjkim2007@gmail.com>
2025-03-10 13:47:56 -0400

    Added comments on JavaScript file and fixed clearing for 8x8 grid

--- static/js/script.js
+++ static/js/script.js
@@ -1,22 +1,36 @@
+//Gets the HTML element with the ID solve
 const button = document.getElementById('solve');
-
+//With this button, if it is clicked, set background color to green. TBH could be done in CSS but idk
 button.addEventListener('click', function() {
     button.style.backgroundColor = "#45a049";
 });
 
-const gridState = Array(10).fill().map(() => Array(10).fill(false));
+/*
+  gridState:
+  Array(8) initializes an array with 8 elements
+  .fill().map() puts another array of 8 elements and makes all values false. Basically creates 2d Array with all false elements
+
+  gridItems gets all HTML elements with the class 'grid-item' (Gridd Cells)
+  blocks hold gets all elements with class block
+*/
+const gridState = Array(8).fill().map(() => Array(8).fill(false));
 const gridItems = document.querySelectorAll('.grid-item');
 const blocks = document.querySelectorAll('.block');
 let currentBlock = null;
 let draggedBlockCell = null;
 let draggedBlockCellIndex = {row: 0, col: 0};
 
+//NEEDS FIXING
+//For each block add an event listener that checks if dragging
 blocks.forEach(block => {
   block.addEventListener('dragstart', (e) => {
     currentBlock = block;
+    //blockCell and draggedBlockCell kind of unnecessary I think
     const blockCell = e.target;
     draggedBlockCell = blockCell;
+    //BLock cells gets all cells from the block thats selected
     const blockCells = Array.from(block.querySelectorAll('.block-cell'));
+    //For each cell in the block check which index the mouse has entered and then enter those values to draggedBlockCellIndex for offset
     blockCells.forEach((blockCell, index) => {
       blockCell.addEventListener('mouseenter', () => {
         draggedBlockCellIndex = {
@@ -27,21 +41,26 @@ blocks.forEach(block => {
     });
   });
 
+  //After dragging has stopped, reset currentBlock and draggedBlockCell
   block.addEventListener('dragend', () => {
     currentBlock = null;
     draggedBlockCell = null;
   });
 });
 
+//For each value in the grid
 gridItems.forEach(item => {
   item.addEventListener('dragover', (e) => {
     e.preventDefault();
   });
 
+  //When a block is dropped on the grid
   item.addEventListener('drop', (e) => {
     const cellId = e.target.id;
+    //Finds the index of what grid by slicing. Ex cell-1-1. Splits each - and gets the number
     const [row, col] = cellId.split('-').slice(1).map(Number);
     
+
     if (currentBlock) {
       const blockCells = currentBlock.querySelectorAll('.block-cell');
       let positions = []
@@ -50,7 +69,7 @@ gridItems.forEach(item => {
       blockCells.forEach((blockCell, index) => {
         const targetRow = row + Math.floor(index / 3) - draggedBlockCellIndex.row;
         const targetCol = col + (index % 3) - draggedBlockCellIndex.col;
-
+        //Checks if move is valid
         if((gridState[targetRow] && gridState[targetRow][targetCol] === false)){
           positions.push([targetRow, targetCol])
         }
@@ -58,7 +77,7 @@ gridItems.forEach(item => {
           valid = false;
         }
       });
-      
+      //if its valid, set the grid value to true and change color of grid
       if(valid){
         for(let i of positions){  
           console.log(i)
@@ -67,14 +86,15 @@ gridItems.forEach(item => {
           document.getElementById(`cell-${rowValid}-${colValid}`).style.backgroundColor = '#90ee90';
         }
       }
-      
+      //Check for cleared rows
       checkAndClearRowsAndColumns();
+      //Send data to flask server
       sendData();
-      
     }
   });
 });
 
+//Flask Server
 function sendData(){
   fetch('/', {
     method: 'POST',
@@ -85,6 +105,7 @@ function sendData(){
   });
 }
 
+//Checks each row and column, if every value in the grid is equal to True, clearRow
 function checkAndClearRowsAndColumns() {
   for (let row = 0; row < gridState.length; row++) {
       if (gridState[row].every(cell => cell === true)) {
@@ -95,17 +116,20 @@ function checkAndClearRowsAndColumns() {
   for (let col = 0; col < gridState[0].length; col++) {
       let fullColumn = true;
       for (let row = 0; row < gridState.length; row++) {
+          //As soon as one value in the column is false, breaks out of loop.
           if (!gridState[row][col]) {
               fullColumn = false;
               break;
           }
       }
+      //If iterates through loop and is still true then column is full and therefore clear
       if (fullColumn) {
           clearColumn(col);
       }
   }
 }
 
+//Clears row by making value from true to false and then making the background empty
 function clearRow(row) {
   for (let col = 0; col < gridState[row].length; col++) {
       gridState[row][col] = false;
@@ -113,6 +137,7 @@ function clearRow(row) {
   }
 }
 
+//Does the same thing for columns
 function clearColumn(col) {
   for (let row = 0; row < gridState.length; row++) {
       gridState[row][col] = false;
